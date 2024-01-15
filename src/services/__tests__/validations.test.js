30c30ce20abc8c2766cbcd8852b52a1fa142fb44
nathanael <nathanael.afangbedjee@student.vinci.be>
2024-01-15 16:55:38 +0100

    atleast 8 characters test is working

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -2,14 +2,14 @@
 const { isValid } = require('../validations');
 
 describe('validations tests suites - isValid', () => {
-    test('should return true or false as the function is defined', () => {
-        const result = isValid();
-        expect(result).toBeDefined();
+    
+    test('should return true if gamerTag length have atleast 8 characters', () => {
+        expect(isValid('MoreThanEigth')).toBe(true);
     });
 
-    test('should return true if the gamertag is valid', () => {
-        const result = isValid('123456789');
-        expect(result).toBe();
-    });    
-});
+    test('should return false if gamerTag length is under 8 characters', () => {
+        expect(isValid('length')).toBe(false);
+    });
 
+    
+});
